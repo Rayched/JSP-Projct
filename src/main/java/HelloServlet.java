558749da9d1a4574e22314638acf14c74acffbe9
Rayched <ny2401@naver.com>
2023-01-26 12:11:13 +0900

    서블릿 라이브러리를 provided 방식ã으로 불러온 후, HelloServlet을 추가, UTF-8 인코딩 설정 추가

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -11,7 +11,12 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().append("Hello World");
+        req.setCharacterEncoding("UTF-8"); //들어오는 데이터를 UTF-8로 해석하겠다.
+        resp.setCharacterEncoding("UTF-8"); //완성되는 HTML의 인코딩을 UTF-8로 설정하겠다.
+        resp.setContentType("text/html; charset-utf-8");
+        //웹 브라우저에게 우리가 만드는 결과물이 UTF-8이라고 알려주는 역할을 한다.
+
+        resp.getWriter().append("안녕하세요.");
     }
 }
 //Hello Servlet 추가
